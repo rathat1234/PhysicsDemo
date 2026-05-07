76456f8269799e0e3cee0ddb9c2bce90b1f9087e
jwn <marajaewoo987@gmail.com>
2026-05-07 17:44:26 +0900

    Day1: 윈도우 생성 + 원 렌더링 + 이동 + 충돌 감지 구현

--- PhysicsDemo/PhysicsDemo.cpp
+++ PhysicsDemo/PhysicsDemo.cpp
@@ -1,180 +1,115 @@
-﻿// PhysicsDemo.cpp : 애플리케이션에 대한 진입점을 정의합니다.
-//
+﻿#include <windows.h>
+#include <cmath>
 
-#include "framework.h"
-#include "PhysicsDemo.h"
+const int WIDTH = 800;
+const int HEIGHT = 600;
 
-#define MAX_LOADSTRING 100
+struct Ball {
+    float x, y;   // 중심 좌표
+    float vx, vy; // 속도
+    float r;      // 반지름
+    COLORREF color;
+};
 
-// 전역 변수:
-HINSTANCE hInst;                                // 현재 인스턴스입니다.
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+Ball balls[2] = {
+    { 150, 300, 3.0f, 0, 50, RGB(220, 50, 50) },   // 빨강: 오른쪽으로 이동
+    { 650, 300, -3.0f, 0, 50, RGB(50, 100, 220) },  // 파랑: 왼쪽으로 이동
+};
 
-// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (msg == WM_PAINT) {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
-{
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+        // 배경 지우기
+        RECT rc; GetClientRect(hwnd, &rc);
+        FillRect(hdc, &rc, (HBRUSH)(COLOR_WINDOW + 1));
 
-    // TODO: 여기에 코드를 입력합니다.
+        // 공 그리기
+        for (auto& b : balls) {
+            HBRUSH brush = CreateSolidBrush(b.color);
+            SelectObject(hdc, brush);
+            Ellipse(hdc, (int)(b.x - b.r), (int)(b.y - b.r),
+                (int)(b.x + b.r), (int)(b.y + b.r));
+            DeleteObject(brush);
+        }
 
-    // 전역 문자열을 초기화합니다.
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_PHYSICSDEMO, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
-
-    // 애플리케이션 초기화를 수행합니다:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
+        EndPaint(hwnd, &ps);
     }
+    if (msg == WM_DESTROY) { PostQuitMessage(0); return 0; }
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PHYSICSDEMO));
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"PhysicsDemo";
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    RegisterClass(&wc);
 
-    MSG msg;
+    HWND hwnd = CreateWindow(L"PhysicsDemo", L"Physics Demo - Day1",
+        WS_OVERLAPPEDWINDOW, 100, 100, WIDTH, HEIGHT,
+        NULL, NULL, hInstance, NULL);
 
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg = {};
+    while (true) {
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            if (msg.message == WM_QUIT) return 0;
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
-    }
 
-    return (int) msg.wParam;
-}
+        // 물체 이동
+        for (auto& b : balls) {
+            b.x += b.vx;
+            b.y += b.vy;
+        }
 
+        // 벽 충돌
+        for (auto& b : balls) {
+            if (b.x - b.r < 0 || b.x + b.r > WIDTH) b.vx *= -1;
+            if (b.y - b.r < 0 || b.y + b.r > HEIGHT) b.vy *= -1;
+        }
 
+        // 공끼리 충돌
+        float dx = balls[1].x - balls[0].x;
+        float dy = balls[1].y - balls[0].y;
+        float dist = sqrtf(dx * dx + dy * dy);
+        if (dist < balls[0].r + balls[1].r && dist > 0) {
+            // 충돌 법선 벡터 (단위 벡터)
+            float nx = dx / dist;
+            float ny = dy / dist;
 
-//
-//  함수: MyRegisterClass()
-//
-//  용도: 창 클래스를 등록합니다.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
+            // 상대 속도
+            float dvx = balls[0].vx - balls[1].vx;
+            float dvy = balls[0].vy - balls[1].vy;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+            // 법선 방향 상대 속도
+            float dot = dvx * nx + dvy * ny;
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PHYSICSDEMO));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_PHYSICSDEMO);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+            // 이미 멀어지고 있으면 스킵
+            if (dot > 0) {
+                balls[0].vx -= dot * nx;
+                balls[0].vy -= dot * ny;
+                balls[1].vx += dot * nx;
+                balls[1].vy += dot * ny;
 
-    return RegisterClassExW(&wcex);
-}
-
-//
-//   함수: InitInstance(HINSTANCE, int)
-//
-//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
-//
-//   주석:
-//
-//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
-//        주 프로그램 창을 만든 다음 표시합니다.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
-
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  용도: 주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
-//  WM_PAINT    - 주 창을 그립니다.
-//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 메뉴 선택을 구문 분석합니다:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
+                // 겹침 해소
+                float overlap = (balls[0].r + balls[1].r - dist) / 2.0f;
+                balls[0].x -= overlap * nx;
+                balls[0].y -= overlap * ny;
+                balls[1].x += overlap * nx;
+                balls[1].y += overlap * ny;
             }
         }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
+
+        InvalidateRect(hwnd, NULL, FALSE);
+        UpdateWindow(hwnd);
+
+        Sleep(16);  // 약 60FPS
     }
     return 0;
-}
-
-// 정보 대화 상자의 메시지 처리기입니다.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
 }
